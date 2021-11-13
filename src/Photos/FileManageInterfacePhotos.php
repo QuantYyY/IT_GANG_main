16ef73dc6cb6501de1bfae88f9d32ad0bca8b1c1
Danil <dmalafeevskii@bk.ru>
2021-11-13 18:18:34 +0300

    Добавление фото

---
+++ src/Photos/FileManageInterfacePhotos.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Photos;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+interface FileManageInterfacePhotos
+{
+    /**
+     * @param UploadedFile $file
+     * @return string
+     */
+    public function imagePostUpload(UploadedFile $file): string;
+
+    /**
+     * @param string $fileName
+     * @return mixed
+     */
+    public function removeImage(string $fileName);
+}
